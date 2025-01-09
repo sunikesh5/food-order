63fe22ffe8ddf071382fd0d4e7b3af7fbb8ab5fa
sunikesh shukla <sunikesh.shukla5@gmail.com>
2025-01-09 14:11:15 -0600

    deploy 44

--- frontend/src/components/MealItem.jsx
+++ frontend/src/components/MealItem.jsx
@@ -14,7 +14,7 @@ export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={`${import.meta.env.VITE_BACKEND_BASE_URL}/public/${meal.image}`} alt={meal.name} />
+        <img src={`${import.meta.env.VITE_BACKEND_BASE_URL}/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">
